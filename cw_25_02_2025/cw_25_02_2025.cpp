f548736292bde71196ec78c64406e62824b177d8
ziffa <rforkaliuk@gmail.com>
2025-02-25 11:46:21 +0200

    Add project files.

---
+++ cw_25_02_2025/cw_25_02_2025.cpp
@@ -0,0 +1,195 @@
+#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+HWND hCombo, hList, hEdit, hRadio1, hRadio2, hRadio3;
+int option = 1;
+int group, student;
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+    // создаём главное окно приложения на основе модального диалога
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+void SetListData();
+void SetGroup();
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CLOSE:
+		EndDialog(hWnd, 0);
+		return TRUE;
+
+	case WM_INITDIALOG:
+		// Получим дескрипторы элементов управления
+		hCombo = GetDlgItem(hWnd, IDC_COMBO1);
+		hList = GetDlgItem(hWnd, IDC_LIST1);
+		hEdit = GetDlgItem(hWnd, IDC_EDIT2);
+		hRadio1 = GetDlgItem(hWnd, IDC_RADIO1);
+
+		SendMessage(hRadio1, BM_SETCHECK, WPARAM(BST_CHECKED), 0);
+
+		// Добавляем времена года в ComboBox
+		SendMessage(hCombo, CB_ADDSTRING, 0, LPARAM(TEXT("GROUP1")));
+		SendMessage(hCombo, CB_ADDSTRING, 0, LPARAM(TEXT("GROUP2")));
+		return TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDC_COMBO1 && HIWORD(wParam) == CBN_SELCHANGE)
+		{
+			group = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			SetGroup();
+			SetListData();
+		}
+		if (LOWORD(wParam) == IDC_LIST1 && HIWORD(wParam) == LBN_SELCHANGE)
+		{
+			int index = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			student = index;
+			int grade = SendMessage(hList, LB_GETITEMDATA, index, 0);
+			TCHAR buffer[100];
+			SendMessage(hList, LB_GETTEXT, index, LPARAM(buffer));
+
+			wsprintf(buffer, TEXT("%d"), grade);
+			SendMessage(hEdit, WM_SETTEXT, 0, LPARAM(buffer));
+		}
+
+		switch (LOWORD(wParam))
+		{
+		case IDC_RADIO1:
+			option = 1;
+			SetListData();
+			break;
+		case IDC_RADIO2:
+			option = 2;
+			SetListData();
+			break;
+		case IDC_RADIO3:
+			option = 3;
+			SetListData();
+			break;
+		}
+
+		return TRUE;
+	}
+	return FALSE;
+}
+
+void SetListData()
+{
+	int index = 0;
+
+	switch (group)
+	{
+	case 0:
+		switch (option)
+		{
+		case 1:
+			SendMessage(hList, LB_SETITEMDATA, index++, 8);
+			SendMessage(hList, LB_SETITEMDATA, index++, 10);
+			SendMessage(hList, LB_SETITEMDATA, index, 11);
+			break;
+		case 2:
+			SendMessage(hList, LB_SETITEMDATA, index++, 10);
+			SendMessage(hList, LB_SETITEMDATA, index++, 8);
+			SendMessage(hList, LB_SETITEMDATA, index, 10);
+			break;
+		case 3:
+			SendMessage(hList, LB_SETITEMDATA, index++, 6);
+			SendMessage(hList, LB_SETITEMDATA, index++, 9);
+			SendMessage(hList, LB_SETITEMDATA, index, 6);
+			break;
+		default:
+			break;
+		}
+		break;
+
+	case 1:
+		switch (option)
+		{
+		case 1:
+			SendMessage(hList, LB_SETITEMDATA, index++, 3);
+			SendMessage(hList, LB_SETITEMDATA, index++, 2);
+			SendMessage(hList, LB_SETITEMDATA, index, 2);
+			break;
+		case 2:
+			SendMessage(hList, LB_SETITEMDATA, index++, 3);
+			SendMessage(hList, LB_SETITEMDATA, index++, 4);
+			SendMessage(hList, LB_SETITEMDATA, index, 7);
+			break;
+		case 3:
+			SendMessage(hList, LB_SETITEMDATA, index++, 4);
+			SendMessage(hList, LB_SETITEMDATA, index++, 5);
+			SendMessage(hList, LB_SETITEMDATA, index, 6);
+			break;
+		default:
+			break;
+		}
+	}
+
+	int grade = SendMessage(hList, LB_GETITEMDATA, student, 0);
+	TCHAR buffer[100];
+	SendMessage(hList, LB_GETTEXT, option, LPARAM(buffer));
+
+	wsprintf(buffer, TEXT("%d"), grade);
+	SendMessage(hEdit, WM_SETTEXT, 0, LPARAM(buffer));
+}
+
+void SetGroup()
+{
+	int index;
+
+	SendMessage(hList, LB_RESETCONTENT, 0, 0);
+
+	switch (group)
+	{
+	case 0:
+		switch (option)
+		{
+		case 1:
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G1_STUDENT1")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G1_STUDENT2")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G1_STUDENT3")));
+			break;
+		case 2:
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G1_STUDENT1")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G1_STUDENT2")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G1_STUDENT3")));
+			break;
+		case 3:
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G1_STUDENT1")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G1_STUDENT2")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G1_STUDENT3")));
+			break;
+		default:
+			break;
+		}
+		break;
+
+	case 1:
+		switch (option)
+		{
+		case 1:
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G2_STUDENT1")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G2_STUDENT2")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G2_STUDENT3")));
+			break;
+		case 2:
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G2_STUDENT1")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G2_STUDENT2")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G2_STUDENT3")));
+			break;
+		case 3:
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G2_STUDENT1")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G2_STUDENT2")));
+			index = SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("G2_STUDENT3")));
+			break;
+		default:
+			break;
+		}
+	}
+}
---
+++ cw_25_02_2025/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by cw_25_02_2025.rc
+//
+#define IDC_MYICON                      2
+#define IDD_CW25022025_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_CW25022025                  107
+#define IDI_SMALL                       108
+#define IDC_CW25022025                  109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_COMBO1                      1000
+#define IDC_LIST1                       1001
+#define IDC_RADIO1                      1002
+#define IDC_RADIO2                      1003
+#define IDC_RADIO3                      1004
+#define IDC_EDIT2                       1006
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
